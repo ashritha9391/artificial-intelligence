383392bf596eb222f23f481f1e04d125f3d3a7ea
ashritha9391 <ashrithachowdari@gmail.com>
2025-03-21 11:07:47 +0530

    Update calender.py

--- calender.py
+++ calender.py
@@ -2,3 +2,14 @@ import calender
 y=int(input("enter year"))
 m=int(input("enter month"))
 print(calender.month(y,m))
+#output
+Enter year: 2025
+Enter month : 3
+     March 2025
+Mo Tu We Th Fr Sa Su
+                1  2
+ 3  4  5  6  7  8  9
+10 11 12 13 14 15 16
+17 18 19 20 21 22 23
+24 25 26 27 28 29 30
+31
