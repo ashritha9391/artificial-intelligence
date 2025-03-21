bd70c9e60836e71af05e8766f4cad262e39c8ee1
ashritha9391 <ashrithachowdari@gmail.com>
2025-03-21 11:05:39 +0530

    Update a star.py

--- a star.py
+++ a star.py
@@ -16,3 +16,5 @@ def astar(graph, start, goal, h):
 graph = {'A': {'B': 1, 'C': 4}, 'B': {'A': 1, 'C': 2, 'D': 5}, 'C': {'A': 4, 'B': 2, 'D': 1}, 'D': {'B': 5, 'C': 1}}
 h = {'A': 7, 'B': 6, 'C': 2, 'D': 0}
 print(astar(graph, 'A', 'D', h))
+#output
+(['A', 'C', 'D'], 5)
